eecb5a6abaefd3d075913017a88d55793870fba6
Justus Lolwerikoi <dev.bojack@gmail.com>
2024-01-03 20:40:36 +0000

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -8,8 +8,4 @@ export default function taskBlock(trueOrFalse) {
     }
 
     return [task, task2];
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 39cc56bd8b4f1770976c3c9bb6b985d35a967f25
--- 0x00-ES6_basic/1-main.js
+++ 0x00-ES6_basic/1-main.js
@@ -1,8 +1,4 @@
 import taskBlock from './1-block-scoped.js';
 
 console.log(taskBlock(true));
-<<<<<<< HEAD
 console.log(taskBlock(false));
-=======
-console.log(taskBlock(false));
->>>>>>> 39cc56bd8b4f1770976c3c9bb6b985d35a967f25
