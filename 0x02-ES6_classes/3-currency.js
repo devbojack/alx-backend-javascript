c3a6c8e521531680721e9c7ca97beb75788de1cc
Justus Lolwerikoi <dev.bojack@gmail.com>
2024-01-04 23:51:11 +0000

    Methods,static,computed

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,37 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = '';
+    this._name = '';
+
+    this._code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this._name}(${this._code})`;
+  }
+}
