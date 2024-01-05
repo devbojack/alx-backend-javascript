9f9f067e2e0dfbbeb7b4c0944d5f95099411b6b0
Justus Lolwerikoi <dev.bojack@gmail.com>
2024-01-05 01:21:11 +0000

    Airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,29 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string' || typeof code !== 'string') {
+      throw new Error();
+    }
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this.code;
+  }
+}
