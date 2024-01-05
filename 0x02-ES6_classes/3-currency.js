bfaafc8e1b8d7cb2d602d00d1b3f4a3fc43eb4b2
Justus Lolwerikoi <dev.bojack@gmail.com>
2024-01-05 00:00:41 +0000

    Error checker

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,27 +1,34 @@
 export default class Currency {
-  constructor(code, name) {
-    if (typeof code !== 'string' || typeof name !== 'string') throw new Error();
-    this.code = code;
-    this.name = name;
+    constructor(code, name) {
+      this._code = code;
+      this._name = name;
+    }
+  
+    get code() {
+      return this._code;
+    }
+  
+    set code(code) {
+      if (typeof code === 'string') {
+        this._code = code;
+      } else {
+        throw new TypeError('Code must be a string');
+      }
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    set name(name) {
+      if (typeof name === 'string') {
+        this._name = name;
+      } else {
+        throw new TypeError('Name must be a string');
+      }
+    }
+  
+    displayFullCurrency() {
+      return `${this._name}(${this._code})`;
+    }
   }
-
-  get code() {
-    return this._code;
-  }
-
-  set code(val) {
-    this._code = val;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  set name(val) {
-    this._name = val;
-  }
-
-  displayFullCurrency() {
-    return `${this.name} (${this.code})`;
-  }
-}
