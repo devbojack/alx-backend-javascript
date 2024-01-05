759b3050392a06666e7c36eee134f61a9533dfdd
Justus Lolwerikoi <dev.bojack@gmail.com>
2024-01-05 17:10:38 +0000

    Multiple promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const photo = uploadPhoto();
+  const user = createUser();
+  return Promise.all(photo, user).then(() => {
+    `${photo.body} ${user.firstName} ${user.lastName}`
+  }
+  ).catch(() => {
+    console.log("Signup system offline");
+  });
+}
