5a93608ffbe5060cb0ecf008631b40910ba6510d
Justus Lolwerikoi <dev.bojack@gmail.com>
2024-01-05 18:40:38 +0000

    Handlemult promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = [];
+  try {
+    const user = await signUpUser(firstName, lastName);
+    results.push({ status: 'Successful', value: user });
+    await uploadPhoto(fileName);
+  } catch (error) {
+    results.push({
+      status: 'Unsuccessful',
+      value: `Error: ${fileName} cannot be proccessed`,
+    });
+  }
+}
