74b1f0e599e48ab890ec272ceeff15e9c8616737
Justus Lolwerikoi <dev.bojack@gmail.com>
2024-01-05 17:18:38 +0000

    Multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,12 @@
+/* eslint-disable import/extensions */
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   const photo = uploadPhoto();
   const user = createUser();
-  return Promise.all(photo, user).then(() => {
-    `${photo.body} ${user.firstName} ${user.lastName}`
-  }
-  ).catch(() => {
-    console.log("Signup system offline");
+  return Promise.all([photo, user]).then((promise) => {
+    console.log(`${promise[0].body} ${promise[1].firstName} ${promise[1].lastName}`);
+  }).catch(() => {
+    console.log('Signup system offline');
   });
 }
